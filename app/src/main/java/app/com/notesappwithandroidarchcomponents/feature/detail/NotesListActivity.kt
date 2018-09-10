1d27598adcef473e43e323a08471659b4deababc
Vrushali <vrushali.raut@go-jek.com>
2018-09-10 23:38:55 +0530

    [Vrushali] Fixed NotesListActivity and import only required things

--- app/src/main/java/app/com/notesappwithandroidarchcomponents/feature/detail/NotesListActivity.kt
+++ app/src/main/java/app/com/notesappwithandroidarchcomponents/feature/detail/NotesListActivity.kt
@@ -83,7 +83,7 @@ class NotesListActivity :AppCompatActivity(), RVAdapterItemClickListener{
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (item?.itemId == R.id.actionSort) {
             showSortDialog()
             true
@@ -95,7 +95,7 @@ class NotesListActivity :AppCompatActivity(), RVAdapterItemClickListener{
         val sortDialogBuilder = AlertDialog.Builder(this@NotesListActivity)
         sortDialogBuilder.setTitle("Sort")
 /*
-        val arrayAdapter = ArrayAdapter<String>(this@NotesListActivity, R.layout.select_dialog_singlechoice)
+        val arrayAdapter = ArrayAdapter<String>(this@NotesListActivity, android.R.layout.select_dialog_singlechoice)
         arrayAdapter.add("Title ascending")
         sortDialogBuilder.setNegativeButton("cancel", { dialog, _ -> dialog.dismiss() })
 
